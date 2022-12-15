409d5bc0072f2bdf6e7eaff6ed9ec54e82f100be
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2022-12-14 16:26:36 -0800

    updated action

--- src/action.js
+++ src/action.js
@@ -1,5 +1,15 @@
+const core = require('@actions/core');
+const github = require("@actions/github");
 async function run() {
-    console.log('hello');
+    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const { context = {} } = github;
+    const { pull_request } = github.getOctokit(GITHUB_TOKEN);
+
+    await octokit.issues.createComment({
+        ...context.repo,
+        issue_number: pull_request.number,
+        body: 'Thankyopu for submitting a pull request! We will try to review this as soon as we can.'
+    })
 }
 
 run();
