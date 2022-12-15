bcc7d9add2af704a209e0b34e24108a73e127ad3
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2022-12-14 16:28:54 -0800

    fxed issue

--- src/action.js
+++ src/action.js
@@ -2,8 +2,10 @@ const core = require('@actions/core');
 const github = require("@actions/github");
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const octokit = github.getOctokit(GITHUB_TOKEN);
+    
     const { context = {} } = github;
-    const { pull_request } = github.getOctokit(GITHUB_TOKEN);
+    const { pull_request } = context.payload;
 
     await octokit.issues.createComment({
         ...context.repo,
