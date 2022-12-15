f3de845e4f6922d82dc987ca495672df4c85f5e0
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2022-12-14 23:09:04 -0800

    INPUT access

--- dist/index.js
+++ dist/index.js
@@ -15866,16 +15866,19 @@ const axios = __nccwpck_require__(8757);
 
 async function run() {
     const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
+    const IAP_TOKEN = core.getInput("IAP_TOKEN");
+    console.log("IAP_TOKEN ", IAP_TOKEN);
 
      axios.post(
        `http://localhost:3211/operations-manager/triggers/manual/` +
          "639a74eafeea2c0226e07b25" +
          "/run?token=" +
-         "OGZjODg2MTM4ZGNmYmU5NWYwYzc2NTQ1MzFlMDQ3MGI="
+         IAP_TOKEN
      ).then(res => {
          console.log(res)
      }).catch(err =>
         console.log(err));
+    
     const octokit = github.getOctokit(GITHUB_TOKEN);
 
     const { context = {} } = github;
