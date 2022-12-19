187bc7f79b9a2ac7f3d3ad9fddb3e94536c72786
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2022-12-19 10:26:07 -0800

    fixed

--- dist/index.js
+++ dist/index.js
@@ -15865,31 +15865,31 @@ const github = __nccwpck_require__(5438);
 const axios = __nccwpck_require__(8757);
 
 async function run() {
-    const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
-    const IAP_TOKEN = core.getInput("IAP_TOKEN");
-    console.log("IAP_TOKEN ", IAP_TOKEN);
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
+  const IAP_TOKEN = core.getInput("IAP_TOKEN");
+  console.log("IAP_TOKEN ", IAP_TOKEN);
 
-
-     axios
-       .post(
-         `https://iap-selab-2021.1-prod.itential.io/workflow_engine/startJobWithOptions/testGithub`+
-           "NzI4MWQ3YjdjNmVjM2I0YjBhN2Q2Y2VlYzAyZjkyYjc=", { options: {} }
-       )
-       .then((res) => {
-         console.log(res);
-       })
-       .catch((err) => console.log(err));
-    
-    const octokit = github.getOctokit(GITHUB_TOKEN);
-
-    const { context = {} } = github;
-    const { pull_request } = context.payload;
-
-    await octokit.rest.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Thankyou for submitting a pull request! We will try to review this as soon as we can. '
+  axios
+    .post(
+      `https://iap-selab-2021.1-prod.itential.io/workflow_engine/startJobWithOptions/testGithub?token=` +
+        "NzI4MWQ3YjdjNmVjM2I0YjBhN2Q2Y2VlYzAyZjkyYjc=",
+      { options: {} }
+    )
+    .then((res) => {
+      console.log(res);
     })
+    .catch((err) => console.log(err));
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thankyou for submitting a pull request! We will try to review this as soon as we can. ",
+  });
 }
 
 run();
