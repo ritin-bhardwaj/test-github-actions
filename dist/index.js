1da6566989bf9a5f0108b200eaa384f9a8fae5c8
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2022-12-21 10:37:50 -0800

    ..

--- dist/index.js
+++ dist/index.js
@@ -15866,48 +15866,59 @@ const axios = __nccwpck_require__(8757);
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-    const IAP_TOKEN = core.getInput("IAP_TOKEN");
-    const IAP_INSTANCE = core.getInput("IAP_INSTANCE");
-    const TIMEOUT = core.getInput("TIMEOUT");
+  const IAP_TOKEN = core.getInput("IAP_TOKEN");
+  const IAP_INSTANCE = core.getInput("IAP_INSTANCE");
+  const TIMEOUT = core.getInput("TIMEOUT");
+  const NO_OF_ATTEMPTS = core.getInput("NO_OF_INPUTS");
+  let count = 0;
 
-    
-    const jobStatus = (job_id) => {
-         axios
-           .get(
-             `${IAP_INSTANCE}/workflow_engine/job/${job_id}/details?token=` +
-               IAP_TOKEN
-           )
-           .then((res) => {
-               console.log('Job Status: ',res.data.status);
-               if (res.data.status === 'running')
-                   setTimeout(() => { 
-                       jobStatus(job_id);
-                   }, TIMEOUT * 1000);
-               else {
-                   axios.get(
-                     `${IAP_INSTANCE}/workflow_engine/job/${job_id}/output?token=` +
-                       IAP_TOKEN
-                   ).then((res) => {
-                       console.log(res.data);
-                   }).catch(err => {
-                       console.log(err);
-                   });
-               }
-           })
-           .catch((err) => {
-             console.log(err);
-           });
-    }
+  const jobStatus = (job_id) => {
+    axios
+      .get(
+        `${IAP_INSTANCE}/workflow_engine/job/${job_id}/details?token=` +
+          IAP_TOKEN
+      )
+      .then((res) => {
+        console.log("Job Status: ", res.data.status);
+          if (res.data.status === "running" && count < NO_OF_ATTEMPTS) {
+              setTimeout(() => {
+                  count += 1;
+              jobStatus(job_id);
+            }, TIMEOUT * 1000);
+        }
+          
+        else if (res.data.status === "complete") {
+          axios
+            .get(
+              `${IAP_INSTANCE}/workflow_engine/job/${job_id}/output?token=` +
+                IAP_TOKEN
+            )
+            .then((res) => {
+              console.log(res.data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else if (res.data.status === "complete") {
+          console.log("Job Canceled");
+        } else {
+          console.log("Workflow Timeout");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   axios
     .post(
       `${IAP_INSTANCE}/workflow_engine/startJobWithOptions/testGithub?token=` +
-       IAP_TOKEN,
+        IAP_TOKEN,
       { options: {} }
     )
     .then((res) => {
-        console.log('Job id: ', res.data._id);
-       jobStatus(res.data._id);
+      console.log("Job id: ", res.data._id);
+      jobStatus(res.data._id);
     })
     .catch((err) => console.log(err));
 
@@ -15924,6 +15935,7 @@ async function run() {
 }
 
 run();
+
 })();
 
 module.exports = __webpack_exports__;
--- src/action.js
+++ src/action.js
@@ -4,48 +4,59 @@ const axios = require("axios");
 
 async function run() {
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN");
-    const IAP_TOKEN = core.getInput("IAP_TOKEN");
-    const IAP_INSTANCE = core.getInput("IAP_INSTANCE");
-    const TIMEOUT = core.getInput("TIMEOUT");
+  const IAP_TOKEN = core.getInput("IAP_TOKEN");
+  const IAP_INSTANCE = core.getInput("IAP_INSTANCE");
+  const TIMEOUT = core.getInput("TIMEOUT");
+  const NO_OF_ATTEMPTS = core.getInput("NO_OF_INPUTS");
+  let count = 0;
 
-    
-    const jobStatus = (job_id) => {
-         axios
-           .get(
-             `${IAP_INSTANCE}/workflow_engine/job/${job_id}/details?token=` +
-               IAP_TOKEN
-           )
-           .then((res) => {
-               console.log('Job Status: ',res.data.status);
-               if (res.data.status === 'running')
-                   setTimeout(() => { 
-                       jobStatus(job_id);
-                   }, TIMEOUT * 1000);
-               else {
-                   axios.get(
-                     `${IAP_INSTANCE}/workflow_engine/job/${job_id}/output?token=` +
-                       IAP_TOKEN
-                   ).then((res) => {
-                       console.log(res.data);
-                   }).catch(err => {
-                       console.log(err);
-                   });
-               }
-           })
-           .catch((err) => {
-             console.log(err);
-           });
-    }
+  const jobStatus = (job_id) => {
+    axios
+      .get(
+        `${IAP_INSTANCE}/workflow_engine/job/${job_id}/details?token=` +
+          IAP_TOKEN
+      )
+      .then((res) => {
+        console.log("Job Status: ", res.data.status);
+          if (res.data.status === "running" && count < NO_OF_ATTEMPTS) {
+              setTimeout(() => {
+                  count += 1;
+              jobStatus(job_id);
+            }, TIMEOUT * 1000);
+        }
+          
+        else if (res.data.status === "complete") {
+          axios
+            .get(
+              `${IAP_INSTANCE}/workflow_engine/job/${job_id}/output?token=` +
+                IAP_TOKEN
+            )
+            .then((res) => {
+              console.log(res.data);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else if (res.data.status === "complete") {
+          console.log("Job Canceled");
+        } else {
+          console.log("Workflow Timeout");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   axios
     .post(
       `${IAP_INSTANCE}/workflow_engine/startJobWithOptions/testGithub?token=` +
-       IAP_TOKEN,
+        IAP_TOKEN,
       { options: {} }
     )
     .then((res) => {
-        console.log('Job id: ', res.data._id);
-       jobStatus(res.data._id);
+      console.log("Job id: ", res.data._id);
+      jobStatus(res.data._id);
     })
     .catch((err) => console.log(err));
 
