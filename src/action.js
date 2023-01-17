f1d56ca76e89e95016d496ea923a4f4c1be4e5bc
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2023-01-17 11:14:45 -0500

    error handling

--- src/action.js
+++ src/action.js
@@ -11,67 +11,133 @@ async function run() {
   const TIMEOUT = core.getInput("TIMEOUT");
   const NO_OF_ATTEMPTS = core.getInput("NO_OF_INPUTS");
   const JOB_STATUS = core.getInput("JOB_STATUS");
-  const JOB_OUTPUT = core.getInput("JOB_OUTPUT");
   let count = 0;
-  const x = { name: "abc", len: 5 }
-  core.setOutput("results", x);
-  console.log("Output", typeof JOB_OUTPUT, JOB_OUTPUT);
-  //check the status of the job and return the output
-  // const jobStatus = (job_id) => {
-  //   axios
-  //     .get(
-  //       `${IAP_INSTANCE}/operations-manager/jobs/${job_id}?token=` + IAP_TOKEN
-  //     )
-  //     .then((res) => {
-  //       console.log("Job Status: ", res.data.data.status);
-  //       if (res.data.data.status === "running" && count < NO_OF_ATTEMPTS) {
-  //         setTimeout(() => {
-  //           count += 1;
-  //           jobStatus(job_id);
-  //         }, TIMEOUT * 1000);
-  //       } else if (res.data.data.status === "complete") {
-  //         core.setOutput("results", res.data.data.variables);
-  //         console.log("Job Output: ", res.data.data.variables);
-  //       } else if (res.data.data.status === "canceled") {
-  //         core.setFailed("Job Canceled");
-  //       } else if (res.data.data.status === "error") {
-  //         core.setFailed(res.data.data.error);
-  //       } else {
-  //         core.setFailed("Job Timeout");
-  //       }
-  //     })
-  //     .catch((err) => {
-  //       core.setFailed(err.response.data);
-  //     });
-  // };
 
-  // //start the job on GitHub event
-  // const startJob = () => {
-  //   axios
-  //     .post(
-  //       `${IAP_INSTANCE}/operations-manager/triggers/endpoint/${API_ENDPOINT}?token=` +
-  //         IAP_TOKEN,
-  //       JSON.parse(API_ENDPOINT_BODY)
-  //     )
-  //     .then((res) => {
-  //       if (JOB_STATUS === "true") jobStatus(res.data.data._id);
-  //     })
-  //     .catch((err) => {
-  //       core.setFailed(err.response.data);
-  //     });
-  // };
-  // startJob();
+ try {
+   //check the status of the job and return the output
+   const jobStatus211 = (job_id) => {
+     axios
+       .get(
+         `${IAP_INSTANCE}/workflow_engine/job/${job_id}/details?token=` +
+           IAP_TOKEN
+       )
+       .then((res) => {
+         console.log("Job Status: ", res.data.status);
+         if (res.data.status === "running" && count < NO_OF_ATTEMPTS) {
+           setTimeout(() => {
+             count += 1;
+             jobStatus211(job_id);
+           }, TIMEOUT * 1000);
+         } else if (res.data.status === "complete") {
+           axios
+             .get(
+               `${IAP_INSTANCE}/workflow_engine/job/${job_id}/output?token=` +
+                 IAP_TOKEN
+             )
+             .then((res) => {
+               core.setOutput("results",res.data.variables);
+             })
+             .catch((err) => {
+               core.setFailed(err.response.data);
+             });
+         } else if (res.data.status === "canceled") {
+           core.setFailed("Job Canceled");
+         } else if (res.data.status === "error") {
+           core.setFailed(res.data.error);
+         } else {
+           core.setFailed("Job Timeout");
+         }
+       })
+       .catch((err) => {
+         core.setFailed(err.response.data);
+       });
+   };
 
-  // const octokit = github.getOctokit(GITHUB_TOKEN);
+   const jobStatus221 = (job_id) => {
+     axios
+       .get(
+         `${IAP_INSTANCE}/operations-manager/jobs/${job_id}?token=` + IAP_TOKEN
+       )
+       .then((res) => {
+         console.log("Job Status: ", res.data.data.status);
+         if (res.data.data.status === "running" && count < NO_OF_ATTEMPTS) {
+           setTimeout(() => {
+             count += 1;
+             jobStatus221(job_id);
+           }, TIMEOUT * 1000);
+         } else if (res.data.data.status === "complete") {
+           core.setOutput("results", res.data.data.variables);
+         } else if (res.data.data.status === "canceled") {
+           core.setFailed("Job Canceled");
+         } else if (res.data.data.status === "error") {
+           core.setFailed(res.data.data.error);
+         } else {
+           core.setFailed("Job Timeout");
+         }
+       })
+       .catch((err) => {
+         core.setFailed(err.response.data);
+       });
+   };
 
-  // const { context = {} } = github;
-  // const { pull_request } = context.payload;
+   //start the job on GitHub event
+   const startJob = () => {
+    axios
+      .get(`${IAP_INSTANCE}/health/server?token=` + IAP_TOKEN)
+      .then((res) => {
+        const release = res.data.release.substring(
+          0,
+          res.data.release.lastIndexOf(".")
+        );
 
-  // await octokit.rest.issues.createComment({
-  //   ...context.repo,
-  //   issue_number: pull_request.number,
-  //   body: "Thankyou for submitting a pull request! We will try to review this as soon as we can. ",
-  // });
+        axios
+          .post(
+            `${IAP_INSTANCE}/operations-manager/triggers/endpoint/${API_ENDPOINT}?token=` +
+              IAP_TOKEN,
+            JSON.parse(API_ENDPOINT_BODY)
+          )
+          .then((res) => {
+            if (JOB_STATUS === "true") {
+              if (release === "2021.1") jobStatus211(res.data._id);
+              else if (release === "2021.2" || release === "2022.1")
+                jobStatus221(res.data.data._id);
+              else
+                core.setFailed(
+                  `This Github Action doesn't support IAP release ${release}`
+                );
+            }
+          })
+          .catch((err) => {
+            core.setFailed(err.response.data);
+          });
+      })
+      .catch((err) => {
+        core.setFailed(err);
+      });
+   };
+
+   startJob();
+ } catch (e) {
+   core.setFailed(e);
+ }
+
+
+
+  const octokit = github.getOctokit(GITHUB_TOKEN);
+
+  const { context = {} } = github;
+  const { pull_request } = context.payload;
+
+  await octokit.rest.issues.createComment({
+    ...context.repo,
+    issue_number: pull_request.number,
+    body: "Thankyou for submitting a pull request! We will try to review this as soon as we can. ",
+  });
 }
 
+
+
 run();
+
+
+ 
