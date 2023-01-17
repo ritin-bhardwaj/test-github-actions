92f292962bc7dd70b1d80b57bf8237e935d75f0c
Ritin Bhardwaj <ritin.bhardwaj@itential.com>
2023-01-17 11:38:40 -0500

    .

--- src/action.js
+++ src/action.js
@@ -13,7 +13,8 @@ async function run() {
   const JOB_STATUS = core.getInput("JOB_STATUS");
   const PREV_JOB_OUTPUT = core.getInput("PREV_JOB_OUTPUT");
   let count = 0;
-  console.log("Previous Job Output",typeof(PREV_JOB_OUTPUT),PREV_JOB_OUTPUT)
+  console.log("Previous Job Output", typeof (PREV_JOB_OUTPUT), PREV_JOB_OUTPUT)
+  console.log(API_ENDPOINT_BODY)
  try {
    //check the status of the job and return the output
    const jobStatus211 = (job_id) => {
